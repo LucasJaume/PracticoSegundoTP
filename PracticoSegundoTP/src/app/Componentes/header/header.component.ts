28597316caa3c419f9afe86c38044715060feac2
matias lencina <mlencinaa3@gmail.com>
2024-10-29 16:29:19 -0300

    nombre y tipo de usuario en header hecho

--- PracticoSegundoTP/src/app/Componentes/header/header.component.ts
+++ PracticoSegundoTP/src/app/Componentes/header/header.component.ts
@@ -9,9 +9,21 @@ import { Location } from '@angular/common';
 })
 export class HeaderComponent {
 
+  nombreUsuario: string | null = null; 
+  tipoUsuario: string | null = null;
+
   constructor(
     private router: Router,
-    private location: Location) {}
+    private location: Location) {this.cargarDatosUsuario();}
+
+    cargarDatosUsuario(): void {
+      const datosUsuario = JSON.parse(sessionStorage.getItem('datosUsuario') || 'null');
+      if (datosUsuario) {
+        // Asumiendo que 'nombre' y 'apellido' están en 'datosUsuario'
+        this.nombreUsuario = `${datosUsuario[0]?.nombre} ${datosUsuario[0]?.apellido}`;
+        this.tipoUsuario = datosUsuario[0]?.rol; // Asegúrate de que 'rol' es el nombre correcto
+      }
+    }
 
   cerrarSesion(): void {
     sessionStorage.clear();
